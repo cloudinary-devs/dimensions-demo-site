1ad752869b4f9f8217704504966548675200c9e6
hezzygershony <hezzy.gershony@cloudinary.com>
2024-11-20 15:59:20 +0200

    feat(demo): add Exercise Bikes

---
+++ static/exercise-bikes/exercise-bikes.js
@@ -0,0 +1,1 @@
+window.getNewPipelineZoomUrl = (number) => `https://dimensions-art.cloudinary.net/d8s-demo-site/image/upload/f_auto/c_fill,h_1000,w_1000,g_auto/p-gym/${number}`;
--- static/red-sneakers/red_sneakers.js
+++ static/red-sneakers/red_sneakers.js
@@ -1,1 +1,1 @@
-window.getNewPipelineZoomUrl = (number) => `https://dimensions-art.cloudinary.net/d8s-demo-site/image/upload/f_auto/p-red-sneakers/${number}.jpg`;
+window.getNewPipelineZoomUrl = (number) => `https://dimensions-art.cloudinary.net/d8s-demo-site/image/upload/f_auto/p-red-sneakers/${number}`;
